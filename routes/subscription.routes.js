a6f81a9db45a20a132513fc467610931887bf1f8
Shivam Soni <sonishivam1402@gmail.com>
2025-05-12 15:51:01 +0530

    Created subscription api's

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import authorize from '../middlewares/auth.middleware.js'
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req,res)=>res.send({title : 'GET all subscriptions'}));
 subscriptionRouter.get('/:id', (req,res)=>res.send({title : 'GET subscription details'}));
-subscriptionRouter.post('/', (req,res)=>res.send({title : 'CREATE new subscription'}));
+subscriptionRouter.post('/', authorize, createSubscription);
 subscriptionRouter.put('/:id', (req,res)=>res.send({title : 'UPDATE subscription details'}));
 subscriptionRouter.delete('/:id', (req,res)=>res.send({title : 'DELETE subscription'}));
-subscriptionRouter.get('/user/:id', (req,res)=>res.send({title : 'GET all user subscriptions'}));
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 subscriptionRouter.put('/:id/cancel', (req,res)=>res.send({title : 'CANCEL subscription'}));
 subscriptionRouter.get('/upcoming-renewals', (req,res)=>res.send({title : 'GET upcoming renewals'}));
 
